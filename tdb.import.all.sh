3f9fad0da6d4d6d3cc5bd67dfbb7fe77f74b9fdf
Sarven Capadisli <info@csarven.ca>
2014-04-23 12:02:51 +0200

    Minor

--- tdb.import.all.sh
+++ tdb.import.all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm /usr/lib/fuseki/DB.interlinks/*
+rm /usr/lib/fuseki/db/interlinks/*
 rm all.nt
 
 for i in *.nt ; do rapper -g "$i" >> all.nt ; done
 sort -u all.nt > foo.nt && mv foo.nt all.nt
-java -Xmx4000M tdb.tdbloader --desc=/usr/lib/fuseki/tdb.interlinks.ttl --graph=http://270a.info/graph/meta all.nt
+java -Xmx8000M tdb.tdbloader --desc=/usr/lib/fuseki/tdb.interlinks.ttl --graph=http://270a.info/graph/meta all.nt
