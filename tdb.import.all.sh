c985b8da433f3c6718196a35c52b63eb04aab56d
Sarven Capadisli <info@csarven.ca>
2014-07-25 21:41:05 +0200

    Slightly faster. Bored

--- tdb.import.all.sh
+++ tdb.import.all.sh
@@ -3,6 +3,6 @@
 rm /usr/lib/fuseki/db/interlinks/*
 rm all.nt
 
-for i in *.nt ; do rapper -g "$i" >> all.nt ; done
-sort -u all.nt > foo.nt && mv foo.nt all.nt
-java -Xmx8000M tdb.tdbloader --desc=/usr/lib/fuseki/tdb.interlinks.ttl --graph=http://270a.info/graph/meta all.nt
+for i in *.nt ; do cat "${i}" >> all.nt; done;
+rapper -g all.nt | sort -u > a.nt && mv a.nt all.nt
+#java -Xmx8000M tdb.tdbloader --desc=/usr/lib/fuseki/tdb.interlinks.ttl --graph=http://270a.info/graph/meta all.nt
